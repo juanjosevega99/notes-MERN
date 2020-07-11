c7ed44012ab482ef1a515feb7fe78b1c5e0fd816
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-11 11:47:41 -0500

    tags navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,27 +5,26 @@ export default class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">
-          NotesApp
-        </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            NotesApp
+          </Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <link className="nav-link" to="/">Notes</link>
+              </li>
+              <li className="nav-item">
+                <link className="nav-link" to="/create">Create Note</link>
+              </li>
+              <li className="nav-item">
+                <link className="nav-link" to="/user">Create User</link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     )
