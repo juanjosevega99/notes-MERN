711f1ed6e13748df84edb87e0ac8593ab6c6aa4f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-18 23:31:09 -0500

    solving challenge

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -3,9 +3,12 @@ import axios from 'axios'
 
 export default class CreateUser extends Component {
 
+  state = {
+    users: []
+  }
   async componentDidMount() {
     const res = await axios.get('http://localhost:4000/api/users')
-    console.log(res)
+    this.setState({ users: res.data })
   }
 
   render() {
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -15,13 +15,13 @@ export default class Navigation extends Component {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <link className="nav-link" to="/">Notes</link>
+                <Link className="nav-link" to="/">Notes</Link>
               </li>
               <li className="nav-item">
-                <link className="nav-link" to="/create">Create Note</link>
+                <Link className="nav-link" to="/create">Create Note</Link>
               </li>
               <li className="nav-item">
-                <link className="nav-link" to="/user">Create User</link>
+                <Link className="nav-link" to="/user">Create User</Link>
               </li>
             </ul>
           </div>
