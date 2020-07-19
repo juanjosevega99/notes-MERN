22e4acb98fdb0c45fb0834d44015f491cb1973a4
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-18 23:40:38 -0500

    refactor

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -4,17 +4,46 @@ import axios from 'axios'
 export default class CreateUser extends Component {
 
   state = {
-    users: []
+    users: [],
+    username: ''
   }
   async componentDidMount() {
     const res = await axios.get('http://localhost:4000/api/users')
     this.setState({ users: res.data })
   }
 
+  onChangeUserName = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        Create User
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card card-body">
+            <h3>Create New User</h3>
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={this.onChangeUserName}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <ul className="list-group">
+            {
+              this.state.users.map(user => (
+                <li className="list-group-item list-group-item-action" key={user._id}>
+                  {user.username}
+                </li>)
+              )
+            }
+          </ul>
+        </div>
       </div>
     )
   }
